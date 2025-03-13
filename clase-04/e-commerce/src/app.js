512f53a5c4a769fe748b2580d942b848f3607000
Luis Mera <luismera86@gamil.com>
2025-03-12 22:15:22 -0300

    clase 4

---
+++ clase-04/e-commerce/src/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { connectMongoDB } from "./config/mongoDB.config.js";
+import routes from "./routes/router.js";
+import envsConfig from "./config/envs.config.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import passport from "./config/passport/passport.config.js";
+
+const app = express();
+
+connectMongoDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: envsConfig.SESSION_SECRET,
+    resave: true, // Evita guardar la sesión si no hay cambios
+    saveUninitialized: true, // Guarda sesiones vacías
+    cookie: { secure: false, maxAge: 500000 }, // Debe estar en true si usas HTTPS
+  })
+);
+
+app.use(cookieParser());
+
+// Inicializar las estrategias de passport
+app.use(passport.initialize());
+
+// Rutas de la api
+app.use("/api", routes);
+
+// Ruta para manejar errores 404
+app.use((req, res) => {
+  res.status(404).json({ error: "Recurso no encontrado" });
+});
+
+app.listen(envsConfig.PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${envsConfig.PORT}`);
+});
---
+++ clase-04/e-commerce/src/routes/auth.routes.js
@@ -0,0 +1,48 @@
+import { Router } from "express";
+import { userDao } from "../persistence/mongo/dao/user.dao.js";
+import { comparePassword, hashPassword } from "../utils/hasPassword.js";
+import { authRole } from "../middlewares/authRole.middleware.js";
+import { createToken } from "../utils/jwt.js";
+import { checkTokenHeader } from "../middlewares/checkTokenHeader.middleware.js";
+import { checkTokenCookie } from "../middlewares/checkTokenCookie.middleware.js";
+import passport from "passport";
+
+const router = Router();
+
+router.post("/login", passport.authenticate("login"), async (req, res) => {
+  try {
+    res.status(200).json({ user: req.user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
+
+router.post("/register", passport.authenticate("register"), async (req, res) => {
+  try {
+    res.status(201).json({ message: req.user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
+
+router.get("/profile", checkTokenCookie, authRole(["admin", "user"]), async (req, res) => {
+  try {
+    res.status(200).json({ user: req.user });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
+
+router.get("/logout", async (req, res) => {
+  try {
+    req.session.destroy();
+    res.status(200).json({ message: "Session cerrada" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
+
+export default router;
